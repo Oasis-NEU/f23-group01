1a31461f2ca39f2c34ba0711e12e03fd076f8526
Alex Rosenthal <alexrosen445@gmail.com>
2023-10-25 21:36:50 -0400

    integrated supabase to event page

--- src/app/events/EventPage.js
+++ src/app/events/EventPage.js
@@ -1,11 +1,38 @@
+"use client";
+import Image from 'next/image'
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
 import Event from '../../components/events-components/Event'
 
+const supabase_URL = process.env.NEXT_PUBLIC_REACT_APP_SUPABASE_URL;
+const supabase_Key = process.env.NEXT_PUBLIC_REACT_APP_SUPABASE_KEY;
+
+export const supabase = createClient(supabase_URL, supabase_Key);
+
 export default function Home() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+      getEvents();
+    }, []);
+
+  async function getEvents() {
+      const { data } = await supabase.from("Events").select();
+      setEvents(data);
+    }
+
   return (
     <div>
       <div class="event-row">
-        <Event />
-        <Event />
+      {events.map((event) => (
+      <Event
+        name={event.name}
+        date={event.date}
+        start_time={event.start_time}
+        end_time={event.end_time}
+        location={event.location}
+        description={event.description}
+      />))}
       </div>
     </div>
   )
