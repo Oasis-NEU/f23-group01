2ba7d5c36250e32ef8adf3181f8baf476950f6a2
Alex Rosenthal <alexrosen445@gmail.com>
2023-11-12 12:54:17 -0500

    login page

---
+++ src/app/login/page.js
@@ -0,0 +1,38 @@
+"use client"
+import { useState, useEffect } from 'react'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { supabase } from '../../../lib/supabase.js'
+import { redirect } from 'next/navigation';
+
+export default function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (<Auth
+              supabaseClient={supabase}
+              queryParams={{
+                access_type: 'offline',
+                prompt: 'consent',
+                hd: 'domain.com',
+              }}
+              appearance={{ theme: ThemeSupa }} />)
+  }
+  else {
+    redirect('/');
+  }
+}
